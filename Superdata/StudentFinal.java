1b818333259571ebd3e160b457691bd1c8540548
nikhilmotivitylabs <106816016+nikhilmotivitylabs@users.noreply.github.com>
2022-07-29 11:00:54 +0530

    updated

---
+++ Superdata/StudentFinal.java
@@ -0,0 +1,17 @@
+package Superdata;
+
+public class StudentFinal {
+
+    final String collegename="srec";
+    //we cannot change value of final
+    final void start(){
+        System.out.println("running..."+collegename);
+    }
+}
+class StudentFinally extends StudentFinal{
+
+    public static void main(String[] args) {
+        new StudentFinally().start();
+    }
+
+}
